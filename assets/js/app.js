133a7eb71b070893c1d1521e2aa649d4e6e5a386
gymunsoed <lf.fikesunsoed@gmail.com>
2026-01-06 11:12:55 +0700

    Add basic login logic

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+const API_URL = "PASTE_URL_APPS_SCRIPT_KAMU_DI_SINI";
+
+function loginMember() {
+  const email = document.getElementById("email").value;
+  const code  = document.getElementById("code").value;
+
+  fetch(`${API_URL}?action=login&email=${email}&code=${code}`)
+    .then(res => res.json())
+    .then(data => {
+      if (data.status === "success") {
+        localStorage.setItem("user", JSON.stringify({
+          role: "member",
+          member_code: data.member_code,
+          name: data.name
+        }));
+        window.location.href = "member.html";
+      } else {
+        document.getElementById("error").innerText = data.message;
+        document.getElementById("error").classList.remove("hidden");
+      }
+    });
+}

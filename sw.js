388039b7f3df35103069c9e8b6f8b0a62dcff097
gymunsoed <lf.fikesunsoed@gmail.com>
2026-01-08 15:53:44 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => response)
+      .catch(() =>
+        new Response("Offline", {
+          status: 200,
+          headers: { "Content-Type": "text/plain" }
+        })
+      )
+  );
 });
